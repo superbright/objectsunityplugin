a9d31d497495a61efb2d92e411b2db62b7b1e773
Nate Turley <turley.nate@gmail.com>
2018-06-20 11:11:52 -0400

    Encapsulate API Endpoints, add textures

--- Scripts/HTTP/ObjectsAPI.cs
+++ Scripts/HTTP/ObjectsAPI.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 namespace SB.Objects.API
 {
+    public static class AWS
+    {
+        public const string AWS_ADDRESS = @"https://s3.amazonaws.com/";
 
- 
+        public const string S3Bucket = "s3deploy.test";
+
+        public const string ThumbnailDir = "img";
+
+        public static string ProjectPath
+        {
+            get { return Path.Combine(AWS.AWS_ADDRESS, AWS.S3Bucket); }
+        }
+
+        public static string ThumbnailRoot
+        {
+            get { return Path.Combine(ProjectPath, AWS.ThumbnailDir); }
+        } 
+    }
 }
+ 
